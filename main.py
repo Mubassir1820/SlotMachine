cf950353e19c5fd9a88746373f1914291cd73338
Mubassir Habib <66573095+Mubassir1820@users.noreply.github.com>
2023-06-03 15:11:24 +0600

    Updated function to fix error in get_slot_machine_spin

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ symbol_count = {
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbol.items():
+    for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
@@ -38,9 +38,11 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i!= len(columns) - 1:
-                print(column[row], ' | ')
+                print(column[row],  end = ' | ')
             else:
-                print(column[row])
+                print(column[row], end = '')
+
+        print()
 
 
 
@@ -101,5 +103,9 @@ def main():
         else:
             break
     print(f'You are betting ${bet} on {lines}. Total bet is equal to: &{total_bet}')
+
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
